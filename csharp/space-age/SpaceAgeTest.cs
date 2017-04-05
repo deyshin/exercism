bd978061643f8a25374ae133e2344b49a2ce159f
Dan Shin <dey.shin@gmail.com>
2017-04-05 14:49:18 -0400

    Remove Test Skip

--- csharp/space-age/SpaceAgeTest.cs
+++ csharp/space-age/SpaceAgeTest.cs
@@ -2,14 +2,14 @@ using Xunit;
 
 public class SpaceAgeTest
 {
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_earth()
     {
         var age = new SpaceAge(1000000000);
         Assert.Equal(31.69, age.OnEarth());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_mercury()
     {
         var age = new SpaceAge(2134835688);
@@ -17,7 +17,7 @@ public class SpaceAgeTest
         Assert.Equal(280.88, age.OnMercury());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_venus()
     {
         var age = new SpaceAge(189839836);
@@ -25,7 +25,7 @@ public class SpaceAgeTest
         Assert.Equal(9.78, age.OnVenus());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_mars()
     {
         var age = new SpaceAge(2329871239);
@@ -33,7 +33,7 @@ public class SpaceAgeTest
         Assert.Equal(39.25, age.OnMars());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_jupiter()
     {
         var age = new SpaceAge(901876382);
@@ -41,7 +41,7 @@ public class SpaceAgeTest
         Assert.Equal(2.41, age.OnJupiter());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_saturn()
     {
         var age = new SpaceAge(3000000000);
@@ -49,7 +49,7 @@ public class SpaceAgeTest
         Assert.Equal(3.23, age.OnSaturn());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_uranus()
     {
         var age = new SpaceAge(3210123456);
@@ -57,7 +57,7 @@ public class SpaceAgeTest
         Assert.Equal(1.21, age.OnUranus());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_neptune()
     {
         var age = new SpaceAge(8210123456);
