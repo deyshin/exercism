9127db5b6900435fb1a1a342b1f9f0dbb2cedba3
deyshin <dey.shin@gmail.com>
2017-04-05 16:47:07 -0400

    Solve/Test Hamming

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Linq;
 
 public static class Hamming
 {
     public static int Compute(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (firstStrand.Length != secondStrand.Length)
+        { throw new Exception($"Length of the First Strand ({firstStrand.Length}) does not match the Length of the Second Strand ({secondStrand.Length})"); }
+        
+        //get all posible string positions
+        var positions = Enumerable.Range(0, firstStrand.Length);  
+
+        //get the positions where the char's are not equal
+        var hammingPositions = positions.Where(p => firstStrand[p] != secondStrand[p]); 
+
+        //return counts of the hamming positions.
+        return hammingPositions.Count();
     }
 }
