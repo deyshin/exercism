e15d14d2d9f8e0a2fc46199b6c73e0985d5b4ed8
Dan Shin <dey.shin@gmail.com>
2017-04-05 13:46:16 -0400

    Fetch/Solve SumOfMultiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int To(IEnumerable<int> multiples, int max)
+    {
+        var nums = Enumerable.Range(1, max - 1);    //max - 1 removes 'max' itself
+        var mults = nums.Where( n => multiples.Any(m => n % m == 0));
+
+        return mults.Sum(); 
+    }
+}
