b9cca03863a3c76a07a311ba1c636cdc05ebde84
Dan Shin <dey.shin@gmail.com>
2017-04-05 14:49:32 -0400

    Solved

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,47 +2,59 @@ using System;
 
 public class SpaceAge
 {
+    private double Seconds;
+    private double ReferenceYearInSeconds = 31557600;
+
     public SpaceAge(long seconds)
     {
+        Seconds = (double)seconds;
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ApplyOrbitalPeriodCompensation(1);
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ApplyOrbitalPeriodCompensation(0.2408467);
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ApplyOrbitalPeriodCompensation(0.61519726);
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ApplyOrbitalPeriodCompensation(1.8808158);
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ApplyOrbitalPeriodCompensation(11.862615);
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ApplyOrbitalPeriodCompensation(29.447498);
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ApplyOrbitalPeriodCompensation(84.016846);
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ApplyOrbitalPeriodCompensation(164.79132);
+    }
+
+    private double ApplyOrbitalPeriodCompensation(double yearRatio)
+    {
+        var earthYear = Seconds/ReferenceYearInSeconds;
+        var convertedYear = earthYear / yearRatio;
+
+        return Math.Round(convertedYear, 2);
     }
 }
