2ec456d75ae8510ec53e159aebb3cf95ef959f8e
Dan Shin <dey.shin@gmail.com>
2017-04-05 13:03:10 -0400

    Submit solution
    I decided to use method chaining in order to perform one task of transforming DNA string to RNA string. I believe C# does not allow method cascading.

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -4,6 +4,14 @@ public static class Complement
 {
     public static string OfDna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var t = 
+            nucleotide
+            .ToLower()
+            .Replace("g", "C")
+            .Replace("c", "G")
+            .Replace("t", "A")
+            .Replace("a", "U");
+            
+        return t;
     }
 }
