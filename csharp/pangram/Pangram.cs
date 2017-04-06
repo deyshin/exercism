837f73a60c874dae2e71233aba9053596412a51d
deyshin <dey.shin@gmail.com>
2017-04-06 15:20:46 -0400

    Solved Pangram

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        var az = 
+            Enumerable
+            .Range('a','z' - 'a')   //ASCII int values
+            .Select(a => (char)a)   //convert to char
+            .ToList();   
+        
+        var t =
+			Regex.Replace(input, @"\s", string.Empty)   //remove whitespace
+            .ToLower()              //unify char case
+			.Distinct()             //remove duplicate
+			.OrderBy(r => r)        //ascending alphabetical order
+            .ToList();
+        
+        return az.Except(t).Count() == 0;
     }
 }
