89d6a9b982a60c4b3b239234ca50c0936692a499
deyshin <dey.shin@gmail.com>
2017-04-07 13:27:24 -0400

    Solve Nucleotide Count

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -3,21 +3,41 @@ using System.Collections.Generic;
 
 public class DNA
 {
-    public DNA(string sequence)
+    private char[] sequence { get; set; }
+    private List<char> ValidNucleotide;
+
+    public DNA(string seq)
     {
+        sequence = seq.ToCharArray();
+        ValidNucleotide = new List<char> { 'A', 'T', 'C', 'G' };
     }
 
     public IDictionary<char, int> NucleotideCounts
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            var nc = new Dictionary<char, int>
+            {
+                {'A', Count('A')},
+                {'T', Count('T')},
+                {'C', Count('C')},
+                {'G', Count('G')},
+            };
+
+            return nc;
         }
     }
 
     public int Count(char nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (ValidNucleotide.Contains(nucleotide) == false)
+        { throw new InvalidNucleotideException(); }
+
+        var count = 0;
+        foreach (var n in sequence)
+        { if (n == nucleotide) { count++; } }
+
+        return count;
     }
 }
 
