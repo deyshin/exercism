21d07bdf3c593707ec69ecae3227d9d6b9da1111
deyshin <dey.shin@gmail.com>
2017-04-07 12:42:48 -0400

    Solved Raindrops

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Linq;
 
 public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var factorsOfNumber = 
+            Enumerable
+            .Range(1, number)
+            .Where(f => number % f == 0);
+        
+        //Analize factorsOfNumber & construct string
+        var raindrops = string.Empty;
+
+        raindrops += factorsOfNumber.Contains(3) ? "Pling" : string.Empty;
+        raindrops += factorsOfNumber.Contains(5) ? "Plang" : string.Empty;
+        raindrops += factorsOfNumber.Contains(7) ? "Plong" : string.Empty;
+        if (raindrops == string.Empty) { raindrops = number.ToString(); }
+        
+        return raindrops;
     }
 }
