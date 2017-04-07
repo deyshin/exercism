9f2b89c7bb61e1649b3bc69389317d16767258c4
deyshin <dey.shin@gmail.com>
2017-04-07 15:38:46 -0400

    Solve Accumulate (2 answers)
    Commented out portion does not lazy load - hence failing 'Accumulate_is_lazy'.
    
    I used Linq to solve this issue. I am not certain if this does not comply with the original requirment.

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class AccumulateExtensions
 {
+    
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // original answer that does NOT lazy loads
+        // var accumulated = new List<U>();
+        // foreach (var e in collection)
+        // { accumulated.Add(func(e)); }
+
+        return collection.Select(i => func(i));
     }
 }
