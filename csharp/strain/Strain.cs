199d863a7e027c33a81194b1c511e404667c6aec
deyshin <dey.shin@gmail.com>
2017-04-13 12:47:58 -0400

    Solve Strain

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return collection.Where(e => predicate(e));
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return collection.Where(e => !predicate(e));
     }
 }
