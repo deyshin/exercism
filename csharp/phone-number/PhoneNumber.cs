34c52e4e3ec5ad6e04cced3f1c4a070894a935ae
Dan Shin <dey.shin@gmail.com>
2017-04-12 22:42:01 -0400

    Solve Phone Numbers

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,24 +1,49 @@
 using System;
+using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
-    public PhoneNumber(string phoneNumber)
+
+    private string badPhoneNumber = "0000000000";
+    private string phoneNumber;
+    public PhoneNumber(string pn)
     {
+        var phoneNum = Regex.Replace(pn, "[^0-9]", "");
+        phoneNumber =
+            CheckPhoneNumberValidity(phoneNum) ?
+            TrimPhoneNumber(phoneNum) : badPhoneNumber;
+    }
+
+    private bool CheckPhoneNumberValidity(string phoneNum)
+    {
+        var l = phoneNum.Length;
+        var isValid =
+            (l == 10)
+            || (l == 11 && phoneNum[0] == '1');
+
+        return isValid;
+    }
+
+    private string TrimPhoneNumber(string phoneNum)
+    {
+        var trimmedPhoneNumber = phoneNum.Length == 11 ?
+         phoneNum.Substring(1,10) : phoneNum;
+
+        return trimmedPhoneNumber;
     }
 
     public string Number
     {
         get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        { return phoneNumber; }
     }
 
     public string AreaCode
     {
         get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        { return phoneNumber.Substring(0, 3); }
     }
+
+    public override string ToString()
+    { return $"({AreaCode}) {phoneNumber.Substring(3,3)}-{phoneNumber.Substring(6,4)}";}
 }
