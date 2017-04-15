4f460f3619711266f892fba212ae1148ebdbf2a1
Dan Shin <dey.shin@gmail.com>
2017-04-15 03:43:23 -0400

    Fetch Proverb

---
+++ csharp/proverb/ProverbTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+public class ProverbTest
+{
+    [Fact]
+    public void Line_one()
+    {
+        Assert.Equal("For want of a nail the shoe was lost.", Proverb.Line(1));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Line_four()
+    {
+        Assert.Equal("For want of a rider the message was lost.", Proverb.Line(4));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Line_seven()
+    {
+        Assert.Equal("And all for the want of a horseshoe nail.", Proverb.Line(7));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void All_lines()
+    {
+        const string expected = "For want of a nail the shoe was lost.\n" +
+                                "For want of a shoe the horse was lost.\n" +
+                                "For want of a horse the rider was lost.\n" +
+                                "For want of a rider the message was lost.\n" +
+                                "For want of a message the battle was lost.\n" +
+                                "For want of a battle the kingdom was lost.\n" +
+                                "And all for the want of a horseshoe nail.";
+
+        Assert.Equal(expected, Proverb.AllLines());
+    }
+}
