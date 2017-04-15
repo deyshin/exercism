a8c18e1da70f9cfafedd16eaeadb86f518d53e0a
Dan Shin <dey.shin@gmail.com>
2017-04-15 03:51:26 -0400

    Solve Proverb

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -2,13 +2,23 @@
 
 public static class Proverb
 {
+    private static string allLines =
+        "For want of a nail the shoe was lost.\n" +
+        "For want of a shoe the horse was lost.\n" +
+        "For want of a horse the rider was lost.\n" +
+        "For want of a rider the message was lost.\n" +
+        "For want of a message the battle was lost.\n" +
+        "For want of a battle the kingdom was lost.\n" +
+        "And all for the want of a horseshoe nail.";
+  
+
     public static string Line(int line)
     {
-        throw new NotImplementedException();
+        return allLines.Split('\n')[line -1];
     }
 
     public static string AllLines()
     {
-        throw new NotImplementedException();
+        return allLines;
     }
 }
