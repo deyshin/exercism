c9dafd399c15fe2b836c065d78981033f39e4445
Dan Shin <dey.shin@gmail.com>
2017-04-15 20:40:06 -0400

    Solve Error Handling

--- csharp/error-handling/ErrorHandling.cs
+++ csharp/error-handling/ErrorHandling.cs
@@ -4,21 +4,46 @@ public static class ErrorHandling
 {
     public static void HandleErrorByThrowingException()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        throw new Exception();
     }
 
     public static int? HandleErrorByReturningNullableType(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int? res;
+        try
+        { res = int.Parse(input); }
+        catch
+        { res = null; }
+
+        return res;
     }
 
     public static bool HandleErrorWithOutParam(string input, out int result)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        bool res;
+
+        try
+        {
+            result = int.Parse(input);
+            res = true;
+        }
+        catch
+        {
+            result = 0;
+            res = false;
+        }
+
+        return res;
     }
 
     public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        try
+        { throw new Exception(); }
+        catch
+        {
+            disposableObject.Dispose();
+            throw;
+        }
     }
 }
