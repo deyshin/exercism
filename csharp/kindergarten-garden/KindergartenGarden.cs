a9b851d77eea3fe984f7e1c6b6d81249a67e902f
Dan Shin <dey.shin@gmail.com>
2017-04-18 23:01:28 -0400

    Stash Solve Kindergarten Garden

--- csharp/kindergarten-garden/KindergartenGarden.cs
+++ csharp/kindergarten-garden/KindergartenGarden.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public enum Plant
 {
@@ -9,19 +10,81 @@ public enum Plant
     Grass
 }
 
+
+
 public class Garden
 {
+    private Dictionary<string, List<Plant>> ChildrenAndPlants { get; set; }
+
     public Garden(IEnumerable<string> children, string windowSills)
     {
+        ChildrenAndPlants = new Dictionary<string, List<Plant>>();
+
+        var SplitSills = windowSills.Split('\n');
+        var top = SplitSills[0].ToCharArray();
+        var bot = SplitSills[1].ToCharArray();
+
+        var cnt = 0;
+        foreach (var c in children)
+        {
+            var plantList = new List<Plant>();
+
+            plantList.Add(TranslateCharToPlant(top[cnt * 2]));
+            plantList.Add(TranslateCharToPlant(top[cnt * 2 + 1]));
+            plantList.Add(TranslateCharToPlant(bot[cnt * 2]));
+            plantList.Add(TranslateCharToPlant(bot[cnt * 2 + 1]));
+
+            ChildrenAndPlants.Add(c, plantList);
+            cnt++;
+            if (cnt + 2 > top.Length / 2)
+            { break; }
+        }
+        Console.WriteLine("===");
+
+        foreach (var i in ChildrenAndPlants)
+        {
+            Console.WriteLine("---" + i.Key);
+            foreach (var v in i.Value)
+            { Console.WriteLine(v.ToString()); }
+        }
+
     }
 
     public IEnumerable<Plant> GetPlants(string child)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<Plant> res = new List<Plant>();
+        ChildrenAndPlants.TryGetValue(child, out res);
+
+        return res;
     }
 
     public static Garden DefaultGarden(string windowSills)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var garden = new Garden(new[] { "Alice", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry" }, windowSills);
+        return garden;
+    }
+
+    private Plant TranslateCharToPlant(char c)
+    {
+        Plant res;
+        switch (c)
+        {
+            case 'V':
+                res = Plant.Violets;
+                break;
+            case 'R':
+                res = Plant.Radishes;
+                break;
+            case 'C':
+                res = Plant.Clover;
+                break;
+            case 'G':
+                res = Plant.Grass;
+                break;
+            default:
+                throw new Exception("Char not recognized");
+        }
+
+        return res;
     }
 }
